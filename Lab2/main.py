7ee36fa0f4545dff7a69c2c3744baae94540877f
Roman <rudin.roman@bk.ru>
2026-02-02 00:19:24 +0200

    Lab2 done

--- Lab2/main.py
+++ Lab2/main.py
@@ -3,52 +3,62 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
 from math import *
+import copy
 
-OUTPUT_EPSILON = '{:0.20f}'
+OUTPUT_EPSILON = '{:0.8f}'
+ZEIDEL_EPSILON = 1e-6
 WORKDIR = Path(__file__).parent
 if not Path.exists(Path(fr'{WORKDIR}\Results')):
     Path.mkdir(Path(fr'{WORKDIR}\Results'))
 
 
-def Gauss(A, b):
-    def find_leading(A, step):
+def find_leading(matrix, b, step) -> tuple[list[list[float]], list[float]]:
         leading_row = step
-        for i in range(step+1, len(A)):
-            if abs(A[i][step]) > abs(A[leading_row][step]):
+        for i in range(step+1, len(matrix)):
+            if abs(matrix[i][step]) > abs(matrix[leading_row][step]):
                 leading_row = i
-        A[leading_row], A[step] = A[step], A[leading_row]
+        matrix[leading_row], matrix[step] = matrix[step], matrix[leading_row]
         b[leading_row], b[step] = b[step], b[leading_row]
-        return A, b
-    
-    for step in range(len(A)-1):
-        A, b = find_leading(A, step)
-        for i in range(len(A)-1, step, -1):
-            division = A[i][step] / A[i-1][step]
-            for j in range(len(A)-1, step-1, -1): 
-                A[i][j] -= A[i-1][j] * division
+        return matrix, b
+
+
+def gauss(matrix, b) -> tuple[list[list[float]], list[float], list[float]]:
+    for step in range(len(matrix)-1):
+        matrix, b = find_leading(copy.deepcopy(matrix), b, step)
+        for i in range(len(matrix)-1, step, -1):
+            division = matrix[i][step] / matrix[i-1][step]
+            for j in range(len(matrix)-1, step-1, -1): 
+                matrix[i][j] -= matrix[i-1][j] * division
             b[i] -= b[i-1] * division
-            A[i][step] = 0
+            matrix[i][step] = 0
 
-    ans = [0] * len(A)
-    for step in range(len(A)-1, -1, -1):
-        ans[step] = (b[step] - sum([A[step][i] * ans[i] for i in range(len(A))])) / A[step][step]
-    return A, ans
+    x = [0] * len(matrix)
+    for step in range(len(matrix)-1, -1, -1):
+        x[step] = (b[step] - sum([matrix[step][i] * x[i] for i in range(len(matrix))])) / matrix[step][step]
+    return matrix, b, x
 
 
 
-def Zeidel(A, b) -> list[int]:
-    functions_array = []
-    for i in range(len(A)):
-        functions_array.append(lambda x: (b[i] - sum([A[i][j] * x[j] for j in range(len(A)) if i != j]) ) / A[i][i])
-    x = [0] * len(A)
-    x_prev = [inf] * len(A)
-    while all([abs(x[i] - x_prev[i]) > 1e-6 for i in range(len(A))]):
-    # for _ in range(4):
-        for i in range(len(A)): x_prev[i] = x[i]
-        for i in range(len(A)):
-            x[i] = functions_array[i](x)
-    return x
-
+def zeidel(matrix, b) -> tuple[bool, list[list[float]], list[float], list[float], int]:
+    for step in range(len(matrix)-1):
+        matrix, b = find_leading(copy.deepcopy(matrix), b, step)
+    print(matrix)
+    checked = all(abs(matrix[i][i]) > sum(abs(matrix[i][j]) for j in range(len(matrix)) if i != j) for i in range(len(matrix)))
+    print(f"Usage of Zeidel is possible:\t{checked}")
+    if not checked:
+        return checked, matrix, b, None, None
+    counter = 0
+    x = [0] * len(matrix)
+    x_prev = [inf] * len(matrix)
+    while all([abs(x[i] - x_prev[i]) > ZEIDEL_EPSILON for i in range(len(matrix))]):
+    # for _ in range(10):
+        x_prev = copy.deepcopy(x)
+        for i in range(len(matrix)):
+            print(x[i], b[i], matrix[i][i], sum([matrix[i][j] * x[j] for j in range(len(matrix)) if i != j]))
+            x[i] = (b[i] - sum([matrix[i][j] * x[j] for j in range(len(matrix)) if i != j])) / matrix[i][i]
+        print(x)
+        counter += 1
+    return checked, matrix, b, x, counter
 
 
 
@@ -62,12 +72,23 @@ if __name__ == '__main__':
     b     = [ -0.4,     3.1,    10.8,    -5.0  ]
     x_true= [  1.0,     2.0,    -1.0,     1.0  ]
 
-    A_gauss, ans_gauss = Gauss(A, b) 
+    A_gauss, b_gauss, ans_gauss = gauss(copy.deepcopy(A), copy.deepcopy(b))
     with open(fr'{WORKDIR}\Results\Guass.csv', 'w') as file:
-        file.write('\n'.join(['\t'.join([OUTPUT_EPSILON.format(item) for item in row]) for row in A_gauss]))
+        file.writelines('\n'.join(['\t'.join([OUTPUT_EPSILON.format(item) for item in A_gauss[i]]) + '\t|\t' + OUTPUT_EPSILON.format(b_gauss[i]) for i in range(len(A))]))
         file.write('\n' * 3)
         file.write('\n'.join([OUTPUT_EPSILON.format(item) for item in ans_gauss]))
+        file.write('\n' * 3)
+        file.write(str(max(abs(np.array(ans_gauss) - np.array(x_true)))))
     
-    ans_zeidel = Zeidel(A, b)
+    zeidel_possible, A_zeidel, b_zeidel, ans_zeidel, num_iter = zeidel(copy.deepcopy(A), copy.deepcopy(b))
     with open(fr'{WORKDIR}\Results\Zeidel.csv', 'w') as file:
+        file.write('Usage of Zeidel is possible:' + '\t' + str(zeidel_possible) + '\n' * 3)
+        file.writelines('\n'.join(['\t'.join([OUTPUT_EPSILON.format(item) for item in A_zeidel[i]]) + '\t|\t' + OUTPUT_EPSILON.format(b_zeidel[i]) for i in range(len(A))]))
+        if not zeidel_possible:
+            exit(0)
+        file.write('\n' * 3)
         file.write('\n'.join([OUTPUT_EPSILON.format(item) for item in ans_zeidel]))
+        file.write('\n' * 3)
+        file.write(str(num_iter))
+        file.write('\n' * 3)
+        file.write(str(max(abs(np.array(ans_zeidel) - np.array(x_true)))))
